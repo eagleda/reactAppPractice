72e42539b9bda97c2679a70ce41bc646b6aa63b7
이다용 <dlekdyd1234@gmail.com>
2024-07-02 09:12:58 +0900

    refactor: 불필요한 render 방지를 위한 shouldComponentUpdate 명시적 변경

--- slidingmenu/src/MenuButton.js
+++ slidingmenu/src/MenuButton.js
@@ -2,6 +2,14 @@ import React,{Component} from "react";
 import "./MenuButton.css";
 
 class MenuButton extends Component{
+    shouldComponentUpdate(nextProps,nextState){
+        if(nextProps.handleMouseDown===this.props.handleMouseDown){ 
+            //전달되는 값이 변하지 않는 값일 경우, 업데이트/re-render 하지 않도록
+            return false;
+        }
+        else return true;
+    }
+
     render(){
         console.log("Rendering: MenuButton");
         return (
