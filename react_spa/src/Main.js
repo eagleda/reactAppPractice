6bdf132abbb5698db031945ec81c602dc32021e0
이다용 <dlekdyd1234@gmail.com>
2024-07-04 16:56:00 +0900

    mainjs'

---
+++ react_spa/src/Main.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+class Main extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Simple SPA</h1>
+                <ul className="header"> {/*unordered list element*/}
+                    
+                </ul> 
+            </div>
+        )
+    }
+}
