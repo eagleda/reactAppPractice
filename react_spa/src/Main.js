bfe45a65c8853205cec98787df0f436502dfe7e3
이다용 <dlekdyd1234@gmail.com>
2024-07-08 16:31:29 +0900

    액티브 된 link list가 하얗게 보이는 기능 푸가

--- react_spa/src/Main.js
+++ react_spa/src/Main.js
@@ -9,17 +9,25 @@ import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
 
+const activeStyle= ({isActive})=>{
+    return{
+        backgroundColor: isActive? "white":undefined,
+        color: isActive? "black":undefined,
+    };
+
+};
 
 class Main extends Component {
+
     render() {
         return (
             <HashRouter>
                 <div>
                     <h1>Simple SPA</h1>
                     <ul className="header"> {/*unordered list element*/}
-                        <li><NavLink to="/">Home</NavLink></li>
-                        <li><NavLink to="/stuff">Stuff</NavLink></li>
-                        <li><NavLink to="/contact">Contact</NavLink></li>
+                        <li><NavLink style={activeStyle} to="/">Home</NavLink></li>
+                        <li><NavLink style={activeStyle} to="/stuff">Stuff</NavLink></li>
+                        <li><NavLink style={activeStyle}to="/contact">Contact</NavLink></li>
                     </ul>
                     <div className="content">
                         <Routes>
