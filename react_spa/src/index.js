51ea819342319eb9dada1f9626f3cbf14f042353
이다용 <dlekdyd1234@gmail.com>
2024-07-08 09:13:41 +0900

    css 추가

--- react_spa/src/index.js
+++ react_spa/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "./Main";
 import { createRoot } from 'react-dom/client';
+import "./index.css"
 const container = document.querySelector("#root");
 const root= createRoot(container);
 root.render(<Main/>);
