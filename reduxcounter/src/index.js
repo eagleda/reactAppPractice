a8a4b83eaa9d3c22015eb617ea0b411bb837da17
이다용 <dlekdyd1234@gmail.com>
2024-07-16 22:56:24 +0900

    index.js react19 식으로 작성

---
+++ reduxcounter/src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { createRoot } from 'react-dom/client';
+import { legacy_createStore as createStore } from "redux"; //이젠 @reduxjs/toolkit에서 제공하는 configureStore
+import { Provider } from "react-redux";
+import counter from "./reducer";
+import App from "./App";
+import "./index.css";
+
+const container = document.querySelector("#root");
+const root= createRoot(container);
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
