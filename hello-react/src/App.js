05d3dd276e572358e25b26f5eda20d3030bf90c0
이다용 <dlekdyd1234@gmail.com>
2024-07-31 08:39:18 +0900

    삼항연산자 예제

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,11 +3,12 @@ import { Fragment } from "react";
 import "./App.css";
 
 function App() {
+  const name = '뉘엑트';
   return (
     //<>=<Fragment>
     <> 
-      <h1>hello React!</h1>
-      <h2>happy hacking?</h2>
+      <h1>name = {name}</h1>
+      {name==='리액트'? (<h1>리액트입니다.</h1>):(<h2>리액트가 아닙니다.</h2>)}
     </>
   );
 }
