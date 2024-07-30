535cee4879356612509bf3864b0c301b8fe148d9
이다용 <dlekdyd1234@gmail.com>
2024-07-31 08:47:55 +0900

    && 연산자 예제

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,13 +3,13 @@ import { Fragment } from "react";
 import "./App.css";
 
 function App() {
-  const name = '뉘엑트';
+  const name = "리액트";
+  const number=0;
   return (
-    //<>=<Fragment>
-    <> 
-      <h1>name = {name}</h1>
-      {name==='리액트'? (<h1>리액트입니다.</h1>):(<h2>리액트가 아닙니다.</h2>)}
-    </>
+    <div>
+      {name==='리액트'&&<h1>리액트입니다</h1>} {/*조건에 맞지 않을 경우 아무것도 보여주지 않아도 되는 경우에는 (선행조건)&&  연산자를 통해 조건부 렌더링 가능*/ }
+      {number && <h1>내용</h1>} {/*주의: falsy한 값인 0을 통해 && 조건부 렌더링을 하면 예외적으로 화면에 0이 나타남*/}
+    </div>
   );
 }
 
