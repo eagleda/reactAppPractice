27792faf35071caa8397870280d27b3eac3b0a6c
이다용 <dlekdyd1234@gmail.com>
2024-07-31 16:58:05 +0900

    undefined 예제 에러를 확인 하지는 못했지만 책의 설명대로 리액트 컴포넌트의 함수가 undefined만 반환하여 렌더링 하는 상황을 만들지 말것.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,16 +1,8 @@
-import logo from "./logo.svg";
-import { Fragment } from "react";
-import "./App.css";
-
+import './App.css';
+ 
 function App() {
-  const name = "리액트";
-  const number=0;
-  return (
-    <div>
-      {name==='리액트'&&<h1>리액트입니다</h1>} {/*조건에 맞지 않을 경우 아무것도 보여주지 않아도 되는 경우에는 (선행조건)&&  연산자를 통해 조건부 렌더링 가능*/ }
-      {number && <h1>내용</h1>} {/*주의: falsy한 값인 0을 통해 && 조건부 렌더링을 하면 예외적으로 화면에 0이 나타남*/}
-    </div>
-  );
+  const name = undefined;
+  return name || '값이 undefined입니다.';
 }
-
+ 
 export default App;
