09bf723a0949b847a58d15b03bf89aa30f718ce3
이다용 <dlekdyd1234@gmail.com>
2024-08-02 15:45:45 +0900

    내요추가

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -8,7 +8,10 @@ function App() {
       <h1>들여쓰기가 이상한</h1>
       <h2>코드</h2>
       <p>입니다.</p>
-      <h2>F1 + format -&gt; 문서 서식 클릭으로 Prettier를 사용하여 코드를 정리해보세요. </h2>
+      <h2>
+        vsCode에서 F1 + format -&gt; 문서 서식 클릭으로 Prettier를 사용하여
+        코드를 정리해보세요.=&gt; shift+ alt+ F{" "}
+      </h2>
       {/*<: &lt;(앰퍼샌드 dlf 티 세미콜론): less than , >: &gt;(앰퍼샌드 지 티 세미콜론): greater than */}
     </div>
   );
