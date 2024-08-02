852132dc099fb9ed4a99af2a36196c870be78721
이다용 <dlekdyd1234@gmail.com>
2024-08-02 09:22:40 +0900

    html서 꺽쇠를 글자로 출력할 때에는 < = &lt; , > = &gt; 로 기입한다.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -9,6 +9,7 @@ function App() {
       <h2>코드</h2>
       <p>입니다.</p>
       <h2>F1 + format -&gt; 문서 서식 클릭으로 Prettier를 사용하여 코드를 정리해보세요. </h2>
+      {/*<: &lt;(앰퍼샌드 dlf 티 세미콜론): less than , >: &gt;(앰퍼샌드 지 티 세미콜론): greater than */}
     </div>
   );
 }
