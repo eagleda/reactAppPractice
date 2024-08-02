573eec6254239ed6445e53ddb78c3a1491c6a96b
이다용 <dlekdyd1234@gmail.com>
2024-08-02 09:02:55 +0900

    시작 태그를 여러 줄로 작성할 때는 그 내부에서 // …과 같은 형태의 주석도 작성 가능

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
       <div className="react">{name}</div>
+      <div
+        className="react" // 시작 태그를 여러 줄로 작성하게 된다면 여기에 주석을 작성할 수 있습니다.
+      > 주석용 div</div>
       <input/> {/* <foo/> :self-closing tag*/}
     </>
   )
