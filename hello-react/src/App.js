c263794a7cb4bac2a504b3d6d25013ac274f631d
이다용 <dlekdyd1234@gmail.com>
2024-08-06 09:10:03 +0900

    함수 컴포넌트 -> 클래스형 컴포넌트로 변경 클래스형 컴포넌트는 state/lifecycle 기능 사용 가능 및 임의 메서드 정의 가능

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,20 +1,10 @@
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  const name = "리액트";
-  return (
-    <div>
-      <div className="react">{name}</div>
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다.</p>
-      <h2>
-        vsCode에서 F1 + format -&gt; 문서 서식 클릭으로 Prettier를 사용하여
-        코드를 정리해보세요.=&gt; shift+ alt+ F{" "}
-      </h2>
-      {/*<: &lt;(앰퍼샌드 dlf 티 세미콜론): less than , >: &gt;(앰퍼샌드 지 티 세미콜론): greater than */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = "react";
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
