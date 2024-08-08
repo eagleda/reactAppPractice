18c261d69254a28ac517ab5e4225bf2073c74013
이다용 <dlekdyd1234@gmail.com>
2024-08-08 09:12:01 +0900

    docs: 오타 수정

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -16,6 +16,6 @@ MyComponent.defaultProps = {
 export default MyComponent;
 //화살표 함수는 함수를 파라미터(인자)로 전달할 때 주로 사용됨
 
-//reaactjs code snippet 확장 프로그램의 rsc/rcc를 사용하여
+//reactjs code snippet 확장 프로그램의 rsc/rcc를 사용하여
 // 함수형/클래스형 컴포넌트를 빠르게 만들 수 있다.
 // snippet? :재사용 가능한 소스 코드, 기계어, 텍스트의 작은 부분
