dd9032aca65d87cdcfadb6e05076100d031d62d2
이다용 <dlekdyd1234@gmail.com>
2024-08-09 08:42:12 +0900

    속성 타입을 정하는 예제

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,12 +1,14 @@
 import PropTypes from "prop-types"; //import 필요
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   //함수 파라미터에서 사용가능한 비구조화 할당|destructuring assignment
   return (
     <div>
       안녕하세요 제 이름은 {name}입니다. <br />
       children 값은 {children}
       입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
