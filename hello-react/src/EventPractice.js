c6e38d8ea62484e707f7f52e83418963c3e0948e
이다용 <dlekdyd1234@gmail.com>
2024-09-09 16:56:59 +0900

    state에 input값 담는 예제

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class EventPractice extends Component {
+  state = {
+    message: "",
+  };
+
   render() {
     return (
       <div>
@@ -9,11 +13,24 @@ class EventPractice extends Component {
           type="text"
           name="massage"
           placeholder="아무거나 입력해 보세요"
+          value={this.state.message}
           onChange={(e) => {
             //e 객체는 SyntheticEvent로 웹 브라우저의 네이티브 이벤트를 감싸는 객체
-            console.log(e.target.value);
+            this.setState({
+              message: e.target.value,
+            });
           }}
         />
+        <button
+          onClick={() => {
+            alert(this.state.message);
+            this.setState({
+              message: "",
+            });
+          }}
+        >
+          확인
+        </button>
       </div>
     );
   }
