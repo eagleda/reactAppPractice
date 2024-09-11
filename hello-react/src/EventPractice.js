af4b63f98e3be887d1f2b0a9bd2035c15065936a
이다용 <dlekdyd1234@gmail.com>
2024-09-11 09:11:12 +0900

    이벤트에 함수 형태의 값을 전달하는 방법을 화살표 함수법에서 분리된 메서드로 변경

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -4,6 +4,24 @@ class EventPractice extends Component {
   state = {
     message: "",
   };
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  }
 
   render() {
     return (
@@ -14,23 +32,9 @@ class EventPractice extends Component {
           name="massage"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={(e) => {
-            //e 객체는 SyntheticEvent로 웹 브라우저의 네이티브 이벤트를 감싸는 객체
-            this.setState({
-              message: e.target.value,
-            });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: "",
-            });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
