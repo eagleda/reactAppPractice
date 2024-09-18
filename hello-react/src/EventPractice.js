4ad3aff7b7450974bfcbe7c331e7d0aaad619b13
이다용 <dlekdyd1234@gmail.com>
2024-09-18 15:29:31 +0900

    함수형 컴포넌트 식으로 구현

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,52 +1,41 @@
-import React, { Component } from "react";
-
-class EventPractice extends Component {
-  state = {
-    user: "",
-    message: "",
+import React, { useState } from "react";
+//함수 컴포넌트식 이벤트 핸들링
+const EventPractice = () => {
+  const [user, setUser] = useState("");
+  const [message, setMessage] = useState("");
+  const onChangeUser = (e) => setUser(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(user + ": " + message);
+    setUser("");
+    setMessage("");
   };
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value, //key : value
-      //key를 [ ]로 감싸면 그 안에 넣은 레퍼런스가 가리키는 실제 값이 key 값으로 사용됨.
-    });
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
   };
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="user"
+        placeholder="사용자명"
+        value={this.state.user}
+        onChange={this.handleChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요"
+        value={this.state.message}
+        onChange={this.handleChange}
+        onKeyDown={this.handleKeyPress}
+      />
+      <button onClick={this.handleClick}>확인</button>
+    </div>
+  );
+};
 
-  handleClick = () => {
-    alert(this.state.user + ": " + this.state.message);
-    this.setState({
-      user: "",
-      message: "",
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input
-          type="text"
-          name="user"
-          placeholder="사용자명"
-          value={this.state.user}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해 보세요"
-          value={this.state.message}
-          onChange={this.handleChange}
-        />
-        <button onClick={this.handleClick}>확인</button>
-      </div>
-    );
-  }
-}
 export default EventPractice;
