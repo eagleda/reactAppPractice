72a259166b9ad378a290bafd438c97884d269849
이다용 <dlekdyd1234@gmail.com>
2024-09-23 09:27:24 +0900

    useState와 e.target을 이용한 구현

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 //함수 컴포넌트식 이벤트 핸들링
 const EventPractice = () => {
-  const [user, setUser] = useState("");
-  const [message, setMessage] = useState("");
-  const onChangeUser = (e) => setUser(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    user: "",
+    message: "",
+  });
+  const { user, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
+
   const onClick = () => {
     alert(user + ": " + message);
-    setUser("");
-    setMessage("");
+    setForm({
+      user: "",
+      message: "",
+    });
   };
   const onKeyPress = (e) => {
     if (e.key === "Enter") {
@@ -23,14 +34,14 @@ const EventPractice = () => {
         name="user"
         placeholder="사용자명"
         value={user}
-        onChange={onChangeUser}
+        onChange={onChange}
       />
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyDown={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
