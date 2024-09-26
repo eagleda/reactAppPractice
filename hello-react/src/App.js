d4e42ed415341660946e0053189ab95d58ae2d8f
이다용 <dlekdyd1234@gmail.com>
2024-09-26 09:22:15 +0900

    ValidationSample 에제

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,13 @@
 import Counter from "./Counter";
 import Say from "./Say";
+import ValidationSample from "./ValidationSample";
 import React from "react";
 import EventPractice from "./EventPractice";
 
 const App = () => {
   return (
     <div>
-      <EventPractice />
+      <ValidationSample />
     </div>
   );
 };
--- hello-react/src/ValidationSample.js
+++ hello-react/src/ValidationSample.js
@@ -10,11 +10,11 @@ class ValidationSample extends Component {
 
   handleChange = (e) => {
     this.setState({
-      password: e.target.password,
+      password: e.target.value,
     });
   };
 
-  handleButtonClick = (e) => {
+  handleButtonClick = () => {
     this.setState({
       clicked: true,
       validated: this.state.password === "0000",
@@ -28,8 +28,17 @@ class ValidationSample extends Component {
           type="password"
           value={this.state.password}
           onChange={this.handleChange}
+          className={
+            this.state.clicked
+              ? this.state.validated
+                ? "success"
+                : "failure"
+              : ""
+          }
         ></input>
+        <button onClick={this.handleButtonClick}>검증하기</button>
       </div>
     );
   }
 }
+export default ValidationSample;
