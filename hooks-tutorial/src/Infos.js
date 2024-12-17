549747661b93e29c78c9f6e9fb4a3e0311eabaae
이다용 <dlekdyd1234@gmail.com>
2024-12-17 16:54:45 +0900

    useReducer을 이용해 다양한 state를 관리하는  Infos 컴포넌트 수정정

--- hooks-tutorial/src/Infos.js
+++ hooks-tutorial/src/Infos.js
@@ -1,32 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  //관리할 상태가 여러 개인 경우
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
 
-  useEffect(() => {
-    console.log("effect");
-    console.log(name);
-    return () => {
-      console.log("cleanup");
-      console.log(name);
-    };
-  }, [name]);
-  //배열 안의 요소: 업데이트의 조건이 되는 값, 비어있을 시, 마운트될 때만 useEffect가 되고 언마운트 될 때만 뒷정리 함수가 작동
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
+  const { name, nickname } = state;
 
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
+  const onChange = (e) => {
+    dispatch(e.target); //dispatch함수는 state를 새러운 값으로 업데이트 하고 리렌더링을 일으킴
+    //dispatch의 유일한 인수는 action, e.target 자체를 액션값으로 사용
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <b>이름:</b> {name}
