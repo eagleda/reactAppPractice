b37cff8c703f30e0e7d632c9c14c478f19bf39a9
eagleda <dlekdyd1234@gmail.com>
2025-02-12 01:03:01 +0900

    reducer를 사용한 useInputs를 통해 인풋을 다루도록 infos.js 수정정

--- hooks-tutorial/src/Infos.js
+++ hooks-tutorial/src/Infos.js
@@ -1,26 +1,12 @@
-import React, { useReducer } from "react";
-
-function reducer(state, action) {
-  //state와 action을 파라미터로 받아와 새로운 상태를 반환하는 함수
-  return {
-    //new state
-    ...state,
-    [action.name]: action.value,
-  };
-}
+import useInputs from "./useInputs";
 
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: "",
     nickname: "", //여러개 한번에
   });
 
   const { name, nickname } = state;
-
-  const onChange = (e) => {
-    dispatch(e.target); //dispatch함수는 state를 새로운 값으로 업데이트 하고 리렌더링을 일으킴
-    //dispatch의 유일한 인수는 action, e.target 자체를 액션값으로 사용
-  };
 
   return (
     <div>
--- hooks-tutorial/src/useInputs.js
+++ hooks-tutorial/src/useInputs.js
@@ -6,3 +6,13 @@ function reducer(state, action) {
     [action.name]: action.value,
   };
 }
+
+export default function useInputs(initialForm) {
+  const [state, dispatch] = useReducer(reducer, initialForm);
+  const onChange = (e) => {
+    dispatch(e.target); //dispatch함수는 state를 새로운 값으로 업데이트 하고 리렌더링을 일으킴
+    //dispatch의 유일한 인수는 action, e.target 자체를 액션값으로 사용
+  };
+  return [state, onChange];
+}
+//useInputs 함수는 useReducer를 사용하여 state와 dispatch 함수를 반환
