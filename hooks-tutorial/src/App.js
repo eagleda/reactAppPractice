65b17a880a8eceb8a76bde6d741167c005d072c9
eagleda <dlekdyd1234@gmail.com>
2025-02-12 00:38:25 +0900

    useMemo, useCallback, useRef 예제제

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <div>
       <Average />
+      <hr />
       <CounterWithReducer />
       <hr />
       <button
--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useCallback, useRef } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값을 계산 중...");
@@ -12,26 +12,40 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
+  }, []); //[]:컴포넌트가 처음 렌더링 될때만 함수를 재생성
 
-  const onInsert = (e) => {
-    if (isNaN(parseInt(number))) {
-      //isNaN : 숫자가 아닌지 확인하는 함수
-      console.log("숫자만 입력 가능합니다.");
-      setNumber("");
-      return;
-    }
-    const nextList = list.concat(parseInt(number)); //parstInt :  문자열을 정수로 변환 , parse: ~를 ..에 맞게 해석/번역하다.
-    setList(nextList);
-    setNumber(""); //number값 초기화
-  };
+  const onInsert = useCallback(
+    (e) => {
+      if (isNaN(parseInt(number))) {
+        //isNaN : 숫자가 아닌지 확인하는 함수
+        console.log("숫자만 입력 가능합니다.");
+        setNumber("");
+        inputEl.current.focus();
+        return;
+      }
+      //입력이 숫자가 아니면 로그 출력후 리턴
+
+      const nextList = list.concat(parseInt(number)); //parstInt :  문자열을 정수로 변환 , parse: ~를 ..에 맞게 해석/번역하다.
+      setList(nextList);
+      setNumber(""); //number값 초기화
+      inputEl.current.focus(); //inputEl.current는 실제 DOM엘리먼트를 가리킴
+    },
+    [number, list]
+  );
+
+  const avg = useMemo(() => getAverage(list), [list]); //useMemo를 사용하여 list 배열이 바뀔 때만 getAverage 함수가 호출되도록 설정
 
   return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input
+        value={number}
+        onChange={onChange}
+        ref={inputEl} /*null이었던 inputEl을 이 input으로 지정*/
+      />
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((value, index) => (
@@ -40,7 +54,7 @@ const Average = () => {
       </ul>
       <div>
         <b>평균값:</b>
-        {getAverage(list)}
+        {avg}
       </div>
     </div>
   );
