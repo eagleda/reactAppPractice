162f418d951edf840c39d085e0fff22c582f68f9
eagleda <dlekdyd1234@gmail.com>
2025-02-12 00:52:49 +0900

    infos.js 구동방식 주석 추가가

--- hooks-tutorial/src/Infos.js
+++ hooks-tutorial/src/Infos.js
@@ -10,13 +10,13 @@ function reducer(state, action) {
 const Info = () => {
   const [state, dispatch] = useReducer(reducer, {
     name: "",
-    nickname: "", //여러개 한번에에
+    nickname: "", //여러개 한번에
   });
 
   const { name, nickname } = state;
 
   const onChange = (e) => {
-    dispatch(e.target); //dispatch함수는 state를 새fh운 값으로 업데이트 하고 리렌더링을 일으킴
+    dispatch(e.target); //dispatch함수는 state를 새로운 값으로 업데이트 하고 리렌더링을 일으킴
     //dispatch의 유일한 인수는 action, e.target 자체를 액션값으로 사용
   };
 
@@ -36,3 +36,4 @@ const Info = () => {
   );
 };
 export default Info;
+//input 입력시 onchange 이벤트 발생 -> onChange함수 호출 -> dispatch함수 호출 -> state 업데이트 -> 리렌더링
